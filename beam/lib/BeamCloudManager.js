9a95d0e8a209a79e9c907ec6985feb2f438354aa
AKash Chauhan <akash6190@gmail.com>
2012-08-11 22:05:32 +0530

    add getSizeRemaining Usage  getSizeRemaining(function(d){console.log(d)},error);

--- beam/lib/BeamCloudManager.js
+++ beam/lib/BeamCloudManager.js
@@ -1,25 +1,28 @@
 /*
-Parent Cloud Manager.
-Acts as the main cloud interface.
-All the implementation of various cloud APIs should extend this
-*/
+ Parent Cloud Manager.
+ Acts as the main cloud interface.
+ All the implementation of various cloud APIs should extend this
+ */
 function BeamCloudInterface() {
 }
-BeamCloudInterface.prototype.uploadFile = function(){}
-BeamCloudInterface.prototype.readFile = function(){}
-BeamCloudInterface.prototype.downloadFileLink = function(){}
 
+BeamCloudInterface.prototype.uploadFile = function() {
+}
+BeamCloudInterface.prototype.readFile = function() {
+}
+BeamCloudInterface.prototype.downloadFileLink = function() {
+}
 /**
-Dropbox API Integration
-**/
-var BeamDropboxManager = function(){
+ Dropbox API Integration
+ **/
+var BeamDropboxManager = function() {
 }
 
-BEAM.extend(BeamDropboxManager,BeamCloudInterface);
+BEAM.extend(BeamDropboxManager, BeamCloudInterface);
 BeamDropboxManager.prototype.uploadFile = function(path, contents, success, error) {
 	var dropbox = new Dropbox(DROPBOX_KEY, DROPBOX_SECRET);
 	dropbox.setDefaultError(error);
-	
+
 	var upload = function() {
 		dropbox.putFileContents(path, contents, success, error);
 	};
@@ -29,7 +32,7 @@ BeamDropboxManager.prototype.uploadFile = function(path, contents, success, erro
 BeamDropboxManager.prototype.readFile = function(path, success, error) {
 	var dropbox = new Dropbox(DROPBOX_KEY, DROPBOX_SECRET);
 	dropbox.setDefaultError(error);
-	
+
 	var contents = function() {
 		dropbox.getFileContents(path, success, error);
 	};
@@ -39,54 +42,60 @@ BeamDropboxManager.prototype.readFile = function(path, success, error) {
 BeamDropboxManager.prototype.downloadFileLink = function(path, success, error) {
 	var dropbox = new Dropbox(DROPBOX_KEY, DROPBOX_SECRET);
 	dropbox.setDefaultError(error);
-	
+
 	var getLink = function() {
 		dropbox.getDirectLink(path, success, error);
 	};
 	dropbox.authorize(getLink, error);
 }
 
+BeamDropboxManager.prototype.getSpaceRemaining = function(success,error) {
+	var dropbox = new Dropbox(DROPBOX_KEY, DROPBOX_SECRET);
+	dropbox.setDefaultError(error);
+	var getSize = function() {
+		var sizeSuc = function(quota){
+			var remaining = (quota.quota_info.quota - quota.quota_info.normal - quota.quota_info.normal)/1024;
+			success(remaining);
+		}
+		 dropbox.getAccountInfo(sizeSuc,error);
+	}
+	dropbox.authorize(getSize, error);
+}
 /**
-Skydrive API Integration
-**/
-var BeamSkydriveManager = function(){
+ Skydrive API Integration
+ **/
+var BeamSkydriveManager = function() {
 
 }
-BEAM.extend(BeamSkydriveManager,BeamCloudInterface);
+BEAM.extend(BeamSkydriveManager, BeamCloudInterface);
 BeamSkydriveManager.prototype.uploadFile = function() {
-
 
 }
 
 BeamSkydriveManager.prototype.downloadFile = function() {
 
-
 }
-
 /**
-Boxnet API Integration
-**/
-var BeamBoxnetManager = function(){
+ Boxnet API Integration
+ **/
+var BeamBoxnetManager = function() {
 
 }
-BEAM.extend(BeamBoxnetManager,BeamCloudInterface);
+BEAM.extend(BeamBoxnetManager, BeamCloudInterface);
 BeamBoxnetManager.prototype.uploadFile = function() {
-
 
 }
 
 BeamBoxnetManager.prototype.downloadFile = function() {
 
-
 }
-
 /**
-  Single access for all the cloud implementations.
-**/
+ Single access for all the cloud implementations.
+ **/
 var BeamCloudManager = (function() {
 	return {
-		dropbox:new BeamDropboxManager(),
-		skydrive:new BeamSkydriveManager(),
-		boxnet:new BeamBoxnetManager()
+		dropbox : new BeamDropboxManager(),
+		skydrive : new BeamSkydriveManager(),
+		boxnet : new BeamBoxnetManager()
 	}
 })();
